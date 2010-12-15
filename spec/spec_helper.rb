7b570df8054414a71a6ab8b448f3377e2587f071
Boba Fat <ivan@workisfun.ru>
2010-12-15 05:28:24 +0300

    clean spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,3 @@
-$:.unshift(File.dirname(__FILE__) + '/../lib')
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+require 'rspec'
 require 'random_text'
-
-begin
-  require 'spec'
-rescue LoadError
-  require 'rubygems'
-  gem 'rspec'
-  require 'spec'
-end
